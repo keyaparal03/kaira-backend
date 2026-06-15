c94ecc053f3c3b0cdf5ef61ad56b2911fd4dd38e
Keya Paral <keyaparal03@gmail.com>
2026-06-16 01:08:50 +0530

    Fixed the cart page

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -1,5 +1,11 @@
 import Cart from "../models/Cart";
 
+/*
+========================================
+GET CART
+========================================
+*/
+
 export const getCart =
 async(
  req:any,
@@ -8,18 +14,28 @@ async(
 
  const cart =
  await Cart.findOne({
+
   user:req.user._id
+
  })
  .populate(
   "items.product"
  );
 
  res.json({
-  success:true,
-  data:cart
- });
 
+  success:true,
+
+  data:cart
+
+ });
 };
+
+/*
+========================================
+ADD TO CART
+========================================
+*/
 
 export const addToCart =
 async(
@@ -27,65 +43,148 @@ async(
  res:any
 )=>{
 
- const {
-  productId,
-  quantity
- } = req.body;
+ try{
 
- let cart =
- await Cart.findOne({
-  user:req.user._id
- });
+  const {
+   productId,
+   quantity
+  } = req.body;
 
- if(!cart){
+  /*
+  MAX QUANTITY
+  */
 
-  cart =
-  await Cart.create({
+  if(quantity > 10){
 
-   user:req.user._id,
+   return res
+   .status(400)
+   .json({
 
-   items:[
-    {
-     product:productId,
-     quantity
-    }
-   ]
+    message:
+    "Maximum quantity is 10"
+
+   });
+  }
+
+  let cart =
+  await Cart.findOne({
+
+   user:req.user._id
 
   });
 
- }else{
+  /*
+  CREATE NEW CART
+  */
 
-  const existingItem =
-  cart.items.find(
-   (item:any)=>
-   item.product.toString()
-   === productId
-  );
+  if(!cart){
 
-  if(existingItem){
+   cart =
+   await Cart.create({
 
-   existingItem.quantity +=
-   quantity;
+    user:req.user._id,
+
+    items:[
+     {
+      product:
+      productId,
+
+      quantity
+     }
+    ]
+
+   });
 
   }else{
 
-   cart.items.push({
-    product:productId,
-    quantity
-   } as any);
+   /*
+   CHECK EXISTING
+   */
 
+   const existingItem =
+   cart.items.find(
+    (item:any)=>
+
+    item.product
+    .toString() ===
+    productId
+   );
+
+   if(existingItem){
+
+    const newQty =
+    existingItem.quantity +
+    quantity;
+
+    if(newQty > 10){
+
+     return res
+     .status(400)
+     .json({
+
+      message:
+      "Maximum quantity is 10"
+
+     });
+    }
+
+    existingItem.quantity =
+    newQty;
+
+   }else{
+
+    cart.items.push({
+
+     product:productId,
+
+     quantity
+
+    } as any);
+   }
+
+   await cart.save();
   }
 
-  await cart.save();
+  /*
+  IMPORTANT
+  REFETCH POPULATED
+  */
 
+  const updatedCart =
+  await Cart.findOne({
+
+   user:req.user._id
+
+  })
+  .populate(
+   "items.product"
+  );
+
+  res.json({
+
+   success:true,
+
+   data:updatedCart
+
+  });
+
+ }catch(error){
+
+  res.status(500)
+  .json({
+
+   message:
+   "Server Error"
+
+  });
  }
-
- res.json({
-  success:true,
-  data:cart
- });
-
 };
+
+/*
+========================================
+UPDATE QUANTITY
+========================================
+*/
 
 export const updateCartItem =
 async(
@@ -93,44 +192,123 @@ async(
  res:any
 )=>{
 
- const {
-  quantity
- } = req.body;
+ try{
 
- const cart =
- await Cart.findOne({
-  user:req.user._id
- });
+  const {
+   quantity
+  } = req.body;
 
- if(!cart){
+  const cart =
+  await Cart.findOne({
 
-  return res.status(404)
-  .json({
-   message:"Cart not found"
+   user:req.user._id
+
   });
 
+  if(!cart){
+
+   return res
+   .status(404)
+   .json({
+
+    message:
+    "Cart not found"
+
+   });
+  }
+
+  /*
+  FIND ITEM
+  */
+
+  const item =
+  cart.items.find(
+   (item:any)=>
+
+   item._id.toString()
+   === req.params.id
+  );
+
+  /*
+  REMOVE IF ZERO
+  */
+
+  if(quantity <= 0){
+
+   cart.items =
+   cart.items.filter(
+    (item:any)=>
+
+    item._id.toString()
+    !== req.params.id
+   ) as any;
+
+  }else{
+
+   /*
+   MAX 10
+   */
+
+   if(quantity > 10){
+
+    return res
+    .status(400)
+    .json({
+
+     message:
+     "Maximum quantity is 10"
+
+    });
+   }
+
+   if(item){
+
+    item.quantity =
+    quantity;
+   }
+  }
+
+  await cart.save();
+
+  /*
+  REFETCH UPDATED
+  */
+
+  const updatedCart =
+  await Cart.findOne({
+
+   user:req.user._id
+
+  })
+  .populate(
+   "items.product"
+  );
+
+  res.json({
+
+   success:true,
+
+   data:updatedCart
+
+  });
+
+ }catch(error){
+
+  res.status(500)
+  .json({
+
+   message:
+   "Server Error"
+
+  });
  }
-
- const item =
- cart.items.find(
-  (item:any)=>
-  item._id.toString()
-  === req.params.id
- );
-
- if(item){
-
-  item.quantity =
-  quantity;
- }
-
- await cart.save();
-
- res.json({
-  success:true
- });
-
 };
+
+/*
+========================================
+REMOVE ITEM
+========================================
+*/
 
 export const removeCartItem =
 async(
@@ -138,31 +316,72 @@ async(
  res:any
 )=>{
 
- const cart =
- await Cart.findOne({
-  user:req.user._id
- });
+ try{
 
- if(!cart){
+  const cart =
+  await Cart.findOne({
 
-  return res.status(404)
-  .json({
-   message:"Cart not found"
+   user:req.user._id
+
   });
 
+  if(!cart){
+
+   return res
+   .status(404)
+   .json({
+
+    message:
+    "Cart not found"
+
+   });
+  }
+
+  /*
+  REMOVE
+  */
+
+  cart.items =
+  cart.items.filter(
+   (item:any)=>
+
+   item._id.toString()
+   !== req.params.id
+
+  ) as any;
+
+  await cart.save();
+
+  /*
+  REFETCH UPDATED
+  */
+
+  const updatedCart =
+  await Cart.findOne({
+
+   user:req.user._id
+
+  })
+  .populate(
+   "items.product"
+  );
+
+  res.json({
+
+   success:true,
+
+   data:updatedCart
+
+  });
+
+ }catch(error){
+
+  res.status(500)
+  .json({
+
+   message:
+   "Server Error"
+
+  });
  }
-
- cart.items =
- cart.items.filter(
-  (item:any)=>
-  item._id.toString()
-  !== req.params.id
- ) as any;
-
- await cart.save();
-
- res.json({
-  success:true
- });
-
 };
